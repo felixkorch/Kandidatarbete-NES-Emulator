384d404ec9700502968d96d775a2c1955b0f93d1
Felix Korch <korch.felix@gmail.com>
2020-02-29 00:14:16 +0100

    Add log.cpp

---
+++ src/llvmes/graphics/log.cpp
@@ -0,0 +1,28 @@
+#include "llvmes/graphics/log.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include <memory>
+
+namespace llvmes {
+
+std::shared_ptr<spdlog::logger> Log::s_logger;
+
+void Log::Init()
+{
+    LLVMES_ASSERT(s_logger == nullptr,
+                  "Trying to initialize the global logger a second time");
+
+    spdlog::set_pattern("%^[%T] %n: %v%$");
+    s_logger = spdlog::stdout_color_mt("LLVMES");
+    s_logger->set_level(spdlog::level::trace);
+}
+
+std::shared_ptr<spdlog::logger> Log::GetLogger()
+{
+    LLVMES_ASSERT(s_logger != nullptr,
+                  "Trying to get logger without initializing it");
+    return s_logger;
+}
+
+}  // namespace llvmes
