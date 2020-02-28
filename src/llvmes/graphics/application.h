ec0f8e43fbe3a0853d15084a6bdde4f9fdc25897
Felix Korch <korch.felix@gmail.com>
2020-02-29 00:13:28 +0100

    Add application.h

---
+++ src/llvmes/graphics/application.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <queue>
+#include <string>
+
+#include "llvmes/graphics/event.h"
+#include "llvmes/graphics/log.h"
+#include "llvmes/graphics/window.h"
+
+namespace llvmes::gfx {
+
+class Application {
+   public:
+    explicit Application(int width = 800, int height = 600,
+                         const std::string& program_name = "Default Title");
+    ~Application();
+
+    virtual void OnImGui();
+    virtual void OnUpdate();
+    virtual void OnEvent(Event& e);
+
+    void Run();
+
+    static Application& Get();
+    Window& GetWindow() { return *m_window; }
+
+   private:
+    bool m_running;
+    std::unique_ptr<Window> m_window;
+    std::queue<Event*> m_event_queue;
+
+   private:
+    static Application* s_application;
+};
+
+}  // namespace llvmes::gfx
