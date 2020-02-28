55fe7fc02bc47077b0114bc707569f6c5e052c8e
Felix Korch <korch.felix@gmail.com>
2020-02-29 00:14:39 +0100

    Add imgui related files

---
+++ src/llvmes/graphics/imgui/imgui_layer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "llvmes/graphics/window.h"
+
+#include <imgui.h>
+
+namespace llvmes::gfx {
+
+// The functions in this class are supposed to be called after
+// creating an OpenGL context with GLFW. The purpose for this class is
+// to hide boilerplate code from this client side and to provide a clean
+// interface for the user.
+class ImGuiLayer {
+   public:
+    static void Create();
+    static void Begin();
+    static void End();
+    static void Destroy();
+    static void SetStyle(ImGuiStyle& style);
+    static void ProcessEvents(Event& e);
+};
+
+}
