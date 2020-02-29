5f65cce0ec8a89b481c850b128c62d81939f5f58
felixkorch <korch.felix@gmail.com>
2020-02-29 21:40:38 +0100

    Remove assert that causes a bug

--- src/llvmes/graphics/log.cpp
+++ src/llvmes/graphics/log.cpp
@@ -10,9 +10,6 @@ std::shared_ptr<spdlog::logger> Log::s_logger;
 
 void Log::Init()
 {
-    LLVMES_ASSERT(s_logger == nullptr,
-                  "Trying to initialize the global logger a second time");
-
     spdlog::set_pattern("%^[%T] %n: %v%$");
     s_logger = spdlog::stdout_color_mt("LLVMES");
     s_logger->set_level(spdlog::level::trace);
